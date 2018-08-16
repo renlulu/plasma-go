4191bb0dae9b99693a58e38d35db9f5d9487000c
renlulu <xiaohuo200@gmail.com>
2018-08-16 13:24:21 +0800

    Fix bug

--- root-chain/contracts/contracts/PlasmaRLP.sol
+++ root-chain/contracts/contracts/PlasmaRLP.sol
@@ -3,35 +3,41 @@ pragma solidity ^0.4.0;
 import "./RLP.sol";
 
 
-/**
- * @title RLPTest
- * @dev Regression tests for RLP contract functions
- */
-contract RLPTest {
-    function eight(bytes tx_bytes)
-    public
-    view
-    returns (uint256, address, address)
-    {
-        var txList = RLP.toList(RLP.toRlpItem(tx_bytes));
-        return (
-        RLP.toUint(txList[5]),
-        RLP.toAddress(txList[6]),
-        RLP.toAddress(txList[7])
-        );
+library PlasmaRLP {
+
+    struct exitingTx {
+        address exitor;
+        address token;
+        uint256 amount;
+        uint256 inputCount;
     }
 
-    function eleven(bytes tx_bytes)
-    public
-    view
-    returns (uint256, address, address, address)
+    /* Public Functions */
+
+    function getUtxoPos(bytes memory challengingTxBytes, uint256 oIndex)
+    internal
+    constant
+    returns (uint256)
     {
-        var txList = RLP.toList(RLP.toRlpItem(tx_bytes));
-        return (
-        RLP.toUint(txList[7]),
-        RLP.toAddress(txList[8]),
-        RLP.toAddress(txList[9]),
-        RLP.toAddress(txList[10])
-        );
+        var txList = RLP.toList(RLP.toRlpItem(challengingTxBytes));
+        uint256 oIndexShift = oIndex * 3;
+        return
+        RLP.toUint(txList[0 + oIndexShift]) +
+        RLP.toUint(txList[1 + oIndexShift]) +
+        RLP.toUint(txList[2 + oIndexShift]);
+    }
+
+    function createExitingTx(bytes memory exitingTxBytes, uint256 oindex)
+    internal
+    constant
+    returns (exitingTx)
+    {
+        var txList = RLP.toList(RLP.toRlpItem(exitingTxBytes));
+        return exitingTx({
+            exitor: RLP.toAddress(txList[7 + 2 * oindex]),
+            token: RLP.toAddress(txList[6]),
+            amount: RLP.toUint(txList[8 + 2 * oindex]),
+            inputCount: RLP.toUint(txList[0]) * RLP.toUint(txList[3])
+            });
     }
 }
